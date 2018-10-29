adaa49cd359c30fb4249f808a15559e3add82745
le-s <steven.an.le@gmail.com>
2018-10-29 14:44:48 -0700

    Create Employee class with #initialize

---
+++ employee.rb
@@ -0,0 +1,10 @@
+class Employee
+  attr_accessor :name, :title, :salary:, :boss
+
+  def intialize(name, title, salary, boss)
+    @name = name
+    @title = title
+    @salary = salary
+    @boss = boss
+  end
+end 
