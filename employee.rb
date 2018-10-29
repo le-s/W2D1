25586328cdef1864d7cdb1cbe75a2c23cad9c68a
le-s <steven.an.le@gmail.com>
2018-10-29 15:06:05 -0700

    Create Manager class with Employee inheritence and #initialize & #bonus

--- employee.rb
+++ employee.rb
@@ -1,10 +1,39 @@
 class Employee
-  attr_accessor :name, :title, :salary:, :boss
+  attr_accessor :name, :title, :salary, :boss
 
-  def intialize(name, title, salary, boss)
+  def initialize(name, title, salary, boss)
     @name = name
     @title = title
     @salary = salary
     @boss = boss
   end
-end 
+
+  def bonus(multiplier)
+    bonus = salary * multiplier
+  end
+end
+
+class Manager < Employee
+  attr_accessor :employees
+  def initialize(name, title, salary, boss, employees=[])
+    super(name, title, salary, boss)
+    @employees = employees
+  end
+
+  def bonus(multiplier)
+    total = 0
+    employees.each { |employee| total += employee.salary }
+    bonus = total * multiplier
+  end
+end
+
+
+
+shawna = Employee.new("Shawna", "TA", 12000, darren)
+david = Employee.new("David", "TA", 10000, darren)
+darren = Manager.new("Darren", "TA Manager", 78000, ned, [shawna, david])
+ned = Manager.new("Ned", "Founder", 1000000, nil, [darren])
+
+puts ned.bonus(5) # => 500_000
+puts darren.bonus(4) # => 88_000
+puts david.bonus(3) # => 30_000
